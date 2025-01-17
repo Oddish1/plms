f976ce99a6648d3f5ed87193ff3ba038bffa775e
Austin Winslow <austin@austinwinslow.com>
2025-01-17 12:47:21 -0800

    added javafx to build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,26 +2,22 @@ group = "com.plms"
 version = "0.01"
 
 plugins {
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
+    id("application")
+	id("org.openjfx.javafxplugin") version "0.1.0" 
+}
+
+javafx {
+		version = "21.0.5"
+		modules = listOf("javafx.controls", "javafx.fxml")
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
-    // Use JUnit Jupiter for testing.
-    testImplementation(libs.junit.jupiter)
-
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is used by the application.
-    implementation(libs.guava)
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
@@ -29,13 +25,7 @@ java {
 }
 
 application {
-    // Define the main class for the application.
     mainClass.set("com.plms.Main")
-}
-
-tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
 }
 
 tasks.named<JavaExec>("run") {
