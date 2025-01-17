c6dd9935de1b31ac185e878978492b51489f7f4a
Austin Winslow <austin@austinwinslow.com>
2025-01-17 13:43:30 -0800

    basic layout setup

--- app/src/main/java/com/plms/Main.java
+++ app/src/main/java/com/plms/Main.java
@@ -1,53 +1,55 @@
 package com.plms;
-import java.util.Scanner;
 
-public class Main{
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+		@Override
+		public void start(Stage primaryStage) {
+
+				// Create Menu Buttons
+				Button viewLibraryButton = new Button("View Library");
+				Button addBookButton = new Button("Add Book");
+
+				// Set up table
+				TableView<Book> tableBooks = new TableView<>();
+
+				TableColumn<Book, String> columnTitle = new TableColumn<>("Title");
+				TableColumn<Book, String> columnAuthor = new TableColumn<>("Author");
+
+				tableBooks.getItems().addAll(
+								new Book("Book Title One", "Author One"),
+								new Book("Book Title Two", "Author Two"),
+								new Book("Book Title Three", "Author Three")
+						);
+
+				columnTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+				columnAuthor.setCellValueFactory(new PropertyValueFactory<>("author"));
+
+				tableBooks.getColumns().addAll(columnTitle, columnAuthor);
+
+				// Set up the layout
+				VBox vbox = new VBox();
+				HBox topControls = new HBox();
+				topControls.getChildren().addAll(addBookButton, viewLibraryButton);
+				vbox.getChildren().addAll(topControls, tableBooks);
+
+				// Create the scene and set it on the stage
+				Scene scene = new Scene(vbox, 800, 600);
+				primaryStage.setTitle("PLMS");
+				primaryStage.setScene(scene);
+				primaryStage.show();
+		}
+
 		public static void main(String[] args) {
-				
-				// Display welcome message
-				System.out.println("Welcome to PLMS!");
-				System.out.println("=========================");
-				
-				// initialize selction before loop start
-				int selection = 0;
-
-				// creat a Scanner object
-				Scanner scanner = new Scanner(System.in);
-
-				//loop as long as selection is not to quit
-				while(selection != 3){
-						// Display menu
-						System.out.println("1. View Library");
-						System.out.println("2. Add Book to Library");
-						System.out.println("3. Quit");
-						System.out.print("[1-3]: ");
-
-						// capture and validate selection
-						if (scanner.hasNextInt()){
-								selection = scanner.nextInt();
-								scanner.nextLine();
-						} else {
-								System.out.println("Invalid input! Please enter a number between 1 and 3.");
-								scanner.nextLine();
-								continue;
-						}
-
-						// determine selection
-						switch (selection) {
-								case 1:
-										System.out.println("You Selected 1");
-										break;
-								case 2:
-										System.out.println("You Selected 2");
-										break;
-								case 3:
-										System.out.println("Goodbye!");
-										break;
-								default:
-										System.out.println("Invalid selection!");
-						}
-				}
-				// close the scanner
-				scanner.close();
+				launch(args);
 		}
 }
